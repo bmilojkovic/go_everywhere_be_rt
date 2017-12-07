48af0b6775cecd60f001f56bae4c1c04a8fdb457
Dusan Kovacevic <dusankovacevic95@gmail.com>
2017-12-07 20:04:43 +0100

    Remove obsolete routes from API

--- src/rest-router.js
+++ src/rest-router.js
@@ -9,94 +9,6 @@ global.activeUsers.ogs = {};
 router.get('/', (request, response) => response.send('REST API is working!'));
 router.post('/', (request, response) => response.json({message: request.body}));
 
-// Assuming that there is a logedin user
-router.get('/:serverID/lobby/all',(request,response)=>{
-  var serverID = request.params.serverID;
-
-
-  if(serverID == 'OGS'){
-
-    var chats = ['global-english']; //add more chats here
-
-    var active_games=[];  //relate to OGS rt api for games
-    var open_games=[];  // this ??
-
-    var onlyRoom = {
-      id: null,
-      chats,
-      active_games,
-      open_games
-    };
-    var rooms = [];
-    rooms.push(onlyRoom);
-
-    //specification object
-    var lobby={
-      id: null,
-      rooms,
-    };
-
-    response.json(lobby).status(200).send();
-
-  }else{
-    response.status(404).json({message:"No such server"}).send();
-  }
-
-});
-
-
-router.get('/server/:serverID/acc/:accID/lobby/:lobbyID/rooms-to-join',(request,response)=>{
-
-  var serverID = request.params.serverID;
-  var accID = request.params.accID;
-  var lobbyID = request.params.lobbyID;
-
-  var user = activeUsers.ogs[accID]; //Getting our user ???
-
-  if(serverID == 'OGS'){
-
-
-    var chats = ['global-english']; //add more chats here
-
-    var active_games=[];  //relate to OGS rt api for games
-    var open_games=[];  // this ??
-
-    var onlyRoom = {
-      id: null,
-      chats,
-      active_games,
-      open_games
-    };
-    var rooms = [];
-    rooms.push(onlyRoom);
-
-
-    response.json(rooms).status(200).send();
-
-  }else{
-    response.status(404).json({message:"No such server"}).send();
-  }
-
-});
-
-
-router.get('/server/:serverID/acc/:accID/lobby/:lobbyID:/joined-rooms',(request,response)=>{
-
-  var serverID = request.params.serverID;
-  var accID = request.params.accID;
-  var lobbyID = request.params.lobbyID;
-
-  if(serverID == 'OGS'){
-
-
-
-
-  }else{
-    response.status(404).json({message:"No such server"}).send();
-  }
-
-});
-
 router.post('/auth', (request, response) => {
   let payload = request.body;
 
@@ -125,4 +37,3 @@ function applyRoutes(app) {
 }
 
 module.exports = applyRoutes;
-
